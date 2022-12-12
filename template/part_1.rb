b9dcf2c9a27fe4be7f6b8b0fe9f3b6b71d760819
Anthony Robertson <arob@hey.com>
2022-12-11 22:44:53 -0500

    updating the template to use the helpers

--- template/part_1.rb
+++ template/part_1.rb
@@ -1,3 +1,15 @@
-File.readlines("input.txt", chomp: true).each do |line|
+require "../../helpers"
 
+module DayX
+  class Solver
+    def initialize(file)
+      @lines = File.readlines(file, chomp: true)
+    end
+
+    def solve
+      # TODO
+    end
+  end
 end
+
+solve(DayX::Solver)
